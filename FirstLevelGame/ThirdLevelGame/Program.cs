b7bae765eb637df008c0e611084745048edd3378
KristineKem <kristine.manina@gmail.com>
2023-07-09 16:44:50 +0300

    Solution name changed, ThirdLevelGame project created

---
+++ FirstLevelGame/ThirdLevelGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ThirdLevelGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
