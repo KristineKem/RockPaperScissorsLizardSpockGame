90df73f4e96509c2d88c45976dd61db7668bf216
KristineKem <kristine.manina@gmail.com>
2023-07-09 15:17:40 +0300

    FirstLevelGame created

---
+++ RockPaperScissorsLizardSpockGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RockPaperScissorsLizardSpockGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
